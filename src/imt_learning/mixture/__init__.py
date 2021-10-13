a5a19e9c8bf4d70cfb7e0db098fdc86b9579a5b8
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-10-12 22:07:25 -0300

    Iniciando repositorio - GaussianMixtureModel

---
+++ src/imt_learning/mixture/__init__.py
@@ -0,0 +1,7 @@
+"""
+O módulo :mod:`imt_learning.mixture` implementa os algoritmos de modelo de misturas.
+"""
+
+from .gaussian_mixture import GaussianMixtureModel
+
+__all__ = ['GaussianMixtureModel']
---
+++ src/imt_learning/mixture/gaussian_mixture.py
@@ -0,0 +1,8 @@
+
+
+class GaussianMixtureModel():
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return 'Modelo de Misturas de Gaussianas'
