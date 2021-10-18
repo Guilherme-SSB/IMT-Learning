bca676b761696df62423d0e3737b82ab7965075d
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-10-18 07:16:39 -0300

    Update gaussian_mixture.py

--- src/imt_learning/mixture/gaussian_mixture.py
+++ src/imt_learning/mixture/gaussian_mixture.py
@@ -1,4 +1,4 @@
-
+# C:\Users\guisa\OneDrive - Instituto Maua de Tecnologia\Livro ML\Aula 01 - Gaussian Mixture Models\Códigos\Gaussian-Mixture-Models\models\estocasticos
 
 class GaussianMixtureModel():
     def __init__(self) -> None:
